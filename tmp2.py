eabd4f12d787ee9c07417cdadebef0388a1dcfa8
Yang Heechan <heechy06@naver.com>
2024-11-05 18:41:43 +0900

    rm ignored files

--- tmp2.py
+++
@@ -1,8 +0,0 @@
-def divide_list(lst, n):
-    k, m = divmod(len(lst), n)
-    return [lst[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n)]
-
-a = [1, 2, 3, 4, 5, 6, 7, 8]
-b = 3
-c = divide_list(a, b)
-print(c)
