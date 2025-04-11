1377be7b5de85d1eb1088b244684cb1b984af275
Yang Heechan <heechy06@naver.com>
2025-04-11 21:50:01 +0900

    RM assets/

--- assets/run.sh
+++
@@ -1,5 +0,0 @@
-cp opencv_4.10.0.zip ../out/opencv_dnn/FL-dataset-opencv_dnn/
-cp opencv_4.10.0.zip ../out/opencv_core/FL-dataset-opencv_core/
-cp opencv_4.10.0.zip ../out/opencv_imgproc/FL-dataset-opencv_imgproc/
-cp opencv_4.10.0.zip ../out/opencv_calib3d/FL-dataset-opencv_calib3d/
-cp opencv_4.10.0.zip ../out/opencv_features2d/FL-dataset-opencv_features2d/
